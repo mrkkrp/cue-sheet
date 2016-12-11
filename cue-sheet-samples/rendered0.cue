b59db382c77d6abf8f0993f795f9bb19f24897c2
mrkkrp <markkarpov@opmbx.org>
2016-12-11 19:45:33 +0300

    Add some tests for render

---
+++ cue-sheet-samples/rendered0.cue
@@ -0,0 +1,32 @@
+CATALOG 1112223334445
+CDTEXTFILE /home/mark/mycdtextfile.txt
+PERFORMER "The Famous Foobar"
+TITLE "Bobla Fett"
+SONGWRITER Paul
+FILE the-famous-foobar-cd1.flac WAVE
+  TRACK 01 AUDIO
+    FLAGS PRE SCMS
+    ISRC agree1234567
+    TITLE "Bobla Fett"
+    PERFORMER "The Famous Foobar"
+    INDEX 01 00:00:15
+  TRACK 02 CDG
+    FLAGS DCP 4CH
+    ISRC agree1234567
+    TITLE "Bobla Fett"
+    PERFORMER "The Famous Foobar"
+    PREGAP 01:00:13
+    INDEX 01 00:00:15
+    POSTGAP 00:08:09
+FILE the-famous-foobar-cd2.mp3 MP3
+  TRACK 03 MODE1/2048
+    ISRC agree1234567
+    TITLE "Bobla Fett"
+    PERFORMER "The Famous Foobar"
+    INDEX 01 00:00:15
+  TRACK 04 MODE2/2336
+    FLAGS DCP 4CH PRE SCMS
+    ISRC agree1234567
+    TITLE "Bobla Fett"
+    PERFORMER "The Famous Foobar"
+    INDEX 01 00:00:15
--- tests/Text/CueSheet/RenderSpec.hs
+++ tests/Text/CueSheet/RenderSpec.hs
@@ -30,11 +30,124 @@
 -- ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 -- POSSIBILITY OF SUCH DAMAGE.
 
+{-# LANGUAGE OverloadedStrings #-}
+
 module Text.CueSheet.RenderSpec
   ( spec )
 where
 
+import Control.Monad.Catch
+import Data.List.NonEmpty (NonEmpty (..))
 import Test.Hspec
+import Test.QuickCheck
+import Text.CueSheet.Render
+import Text.CueSheet.Types
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.List.NonEmpty   as NE
 
 spec :: Spec
-spec = return ()
+spec =
+  describe "renderCueSheet" $ do
+    it "doesn't ever produce trailing whitespace" $
+      property $ \csrf cueSheet -> do
+        let r = renderCueSheet csrf cueSheet
+            f x = if csrf then BL.take (BL.length x - 1) x else x
+        BL.split 10 r `shouldNotSatisfy`
+          any ((" " `BL.isSuffixOf`) . f)
+    it "always ends with the specified new line sequence" $
+      property $ \csrf cueSheet -> do
+        let r   = renderCueSheet csrf cueSheet
+            eol = if csrf then "\r\n" else "\n"
+        r `shouldSatisfy` (eol `BL.isSuffixOf`)
+    it "doesn't look too bad" $ do
+      expected <- BL.readFile "cue-sheet-samples/rendered0.cue"
+      cueSheet <- testCueSheet
+      BL.writeFile "cue-sheet-samples/actual.cue" (renderCueSheet False cueSheet)
+      renderCueSheet False cueSheet `shouldBe` expected
+
+-- | A manually constructed testing CUE sheet.
+
+testCueSheet :: MonadThrow m => m CueSheet
+testCueSheet = do
+  mcn        <- mkMcn "1112223334445"
+  performer  <- mkCueText "The Famous Foobar"
+  title      <- mkCueText "Bobla Fett"
+  songwriter <- mkCueText "Paul"
+  isrc       <- mkIsrc    "agree1234567"
+  pregap     <- fromMmSsFf 1 0 13
+  index      <- fromMmSsFf 0 0 15
+  postgap    <- fromMmSsFf 0 8 9
+  return CueSheet
+    { cueCatalog    = Just mcn
+    , cueCdTextFile = Just "/home/mark/mycdtextfile.txt"
+    , cuePerformer  = Just performer
+    , cueTitle      = Just title
+    , cueSongwriter = Just songwriter
+    , cueFirstTrackNumber = 1
+    , cueFiles      = NE.fromList
+      [ CueFile
+        { cueFileName   = "the-famous-foobar-cd1.flac"
+        , cueFileType   = Wave
+        , cueFileTracks = NE.fromList
+          [ CueTrack
+            { cueTrackDigitalCopyPermitted = False
+            , cueTrackFourChannelAudio     = False
+            , cueTrackPreemphasisEnabled   = True
+            , cueTrackSerialCopyManagement = True
+            , cueTrackType                 = CueTrackAudio
+            , cueTrackIsrc                 = Just isrc
+            , cueTrackTitle                = Just title
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Nothing
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = index :| []
+            , cueTrackPostgap              = Nothing }
+          , CueTrack
+            { cueTrackDigitalCopyPermitted = True
+            , cueTrackFourChannelAudio     = True
+            , cueTrackPreemphasisEnabled   = False
+            , cueTrackSerialCopyManagement = False
+            , cueTrackType                 = CueTrackCdg
+            , cueTrackIsrc                 = Just isrc
+            , cueTrackTitle                = Just title
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Just pregap
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = index :| []
+            , cueTrackPostgap              = Just postgap } ] }
+      , CueFile
+        { cueFileName   = "the-famous-foobar-cd2.mp3"
+        , cueFileType   = MP3
+        , cueFileTracks = NE.fromList
+          [ CueTrack
+            { cueTrackDigitalCopyPermitted = False
+            , cueTrackFourChannelAudio     = False
+            , cueTrackPreemphasisEnabled   = False
+            , cueTrackSerialCopyManagement = False
+            , cueTrackType                 = CueTrackMode1_2048
+            , cueTrackIsrc                 = Just isrc
+            , cueTrackTitle                = Just title
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Nothing
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = index :| []
+            , cueTrackPostgap              = Nothing }
+          , CueTrack
+            { cueTrackDigitalCopyPermitted = True
+            , cueTrackFourChannelAudio     = True
+            , cueTrackPreemphasisEnabled   = True
+            , cueTrackSerialCopyManagement = True
+            , cueTrackType                 = CueTrackMode2_2336
+            , cueTrackIsrc                 = Just isrc
+            , cueTrackTitle                = Just title
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Nothing
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = index :| []
+            , cueTrackPostgap              = Nothing } ] }
+      ]
+    }
