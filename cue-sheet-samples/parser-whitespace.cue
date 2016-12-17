2af482344613ad99ded920304a3a01b37d0ea07c
mrkkrp <markkarpov@opmbx.org>
2016-12-17 20:21:16 +0300

    Add some parser tests

---
+++ cue-sheet-samples/parser-whitespace.cue
@@ -0,0 +1,13 @@
+
+
+
+ PERFORMER   "Faithless" 
+  REM   GENRE  Electronica
+ TITLE "Live in Berlin"    
+FILE   "Faithless - Live in Berlin.mp3" MP3   
+  TRACK  01    AUDIO
+     TITLE "Reverence"  
+   PERFORMER   "Faithless"
+     INDEX 01    00:00:03
+ INDEX 02  00:02:01
+
--- tests/Text/CueSheet/ParserSpec.hs
+++ tests/Text/CueSheet/ParserSpec.hs
@@ -30,11 +30,233 @@
 -- ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 -- POSSIBILITY OF SUCH DAMAGE.
 
+{-# LANGUAGE OverloadedStrings #-}
+
 module Text.CueSheet.ParserSpec
   ( spec )
 where
 
+import Control.Monad.Catch
+import Data.Monoid ((<>))
 import Test.Hspec
+import Test.Hspec.Megaparsec
+import Text.CueSheet.Parser
+import Text.CueSheet.Types
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.List.NonEmpty   as NE
 
 spec :: Spec
-spec = return ()
+spec =
+  describe "parseCueSheet" $ do
+    it "correctly deals with whitespace" $ do
+      bs <- BL.readFile "cue-sheet-samples/parser-whitespace.cue"
+      sheet <- testSheet
+      parseCueSheet "" bs `shouldParse` sheet
+    it "parses commands case-insensitively" $ do
+      bs <- BL.readFile "cue-sheet-samples/parser-case-insensitivity.cue"
+      sheet <- testSheet
+      parseCueSheet "" bs `shouldParse` sheet
+    it "handles comments properly" $ do
+      bs <- BL.readFile "cue-sheet-samples/parser-comments.cue"
+      sheet <- testSheet
+      parseCueSheet "" bs `shouldParse` sheet
+    it "at least one file should be declared" $ do
+      bs <- BL.readFile "cue-sheet-samples/parser-missing-file.cue"
+      let es = foldMap etoks
+            [ "CATALOG"
+            , "CDTEXTFILE"
+            , "FILE"
+            , "PERFORMER"
+            , "REM"
+            , "SONGWRITER"
+            , "TITLE" ]
+      parseCueSheet "" bs `shouldFailWith`
+        err (posN (70 :: Int) bs) (ueof <> es)
+    it "at least one track should be declared" $ do
+      bs <- BL.readFile "cue-sheet-samples/parser-missing-track.cue"
+      parseCueSheet "" bs `shouldFailWith`
+        err (posN (110 :: Int) bs) (ueof <> etoks "REM" <> etoks "TRACK")
+    it "at least one non-zero index should be declared" $ do
+      bs <- BL.readFile "cue-sheet-samples/parser-missing-index.cue"
+      let inTrack1 = cstm (Eec (Just 1) Nothing)
+      parseCueSheet "" bs `shouldFailWith`
+        err (posN (180 :: Int) bs)
+          (ueof <> etoks "INDEX" <> etoks "REM" <> inTrack1)
+    it "parser a normal CUE file without issues" $ do
+      bs <- BL.readFile "cue-sheet-samples/normal.cue"
+      sheet <- normalCueSheet
+      parseCueSheet "" bs `shouldParse` sheet
+
+-- TODO One test per special failure mode
+
+testSheet :: MonadThrow m => m CueSheet
+testSheet = do
+  performer  <- mkCueText "Faithless"
+  title      <- mkCueText "Live in Berlin"
+  trackTitle <- mkCueText "Reverence"
+  return CueSheet
+    { cueCatalog    = Nothing
+    , cueCdTextFile = Nothing
+    , cuePerformer  = Just performer
+    , cueTitle      = Just title
+    , cueSongwriter = Nothing
+    , cueFirstTrackNumber = 1
+    , cueFiles      = NE.fromList
+      [ CueFile
+        { cueFileName = "Faithless - Live in Berlin.mp3"
+        , cueFileType = MP3
+        , cueFileTracks = NE.fromList
+          [ CueTrack
+            { cueTrackDigitalCopyPermitted = False
+            , cueTrackFourChannelAudio     = False
+            , cueTrackPreemphasisEnabled   = False
+            , cueTrackSerialCopyManagement = False
+            , cueTrackType                 = CueTrackAudio
+            , cueTrackIsrc                 = Nothing
+            , cueTrackTitle                = Just trackTitle
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Nothing
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = NE.fromList
+              [CueTime 3, CueTime 151]
+            , cueTrackPostgap              = Nothing } ] } ] }
+
+normalCueSheet :: MonadThrow m => m CueSheet
+normalCueSheet = do
+  performer   <- mkCueText "Faithless"
+  catalog     <- mkMcn "1112223334445"
+  title       <- mkCueText "Live in Berlin"
+  track1Title <- mkCueText "Reverence"
+  track2Title <- mkCueText "She's My Baby"
+  track3Title <- mkCueText "Take the Long Way Home"
+  track4Title <- mkCueText "Insomnia"
+  track5Title <- mkCueText "Bring the Family Back"
+  track6Title <- mkCueText "Salva Mea"
+  track7Title <- mkCueText "Dirty Old Man"
+  track8Title <- mkCueText "God Is a DJ"
+  return CueSheet
+    { cueCatalog    = Just catalog
+    , cueCdTextFile = Just "blah-blah.txt"
+    , cuePerformer  = Just performer
+    , cueTitle      = Just title
+    , cueSongwriter = Nothing
+    , cueFirstTrackNumber = 1
+    , cueFiles      = NE.fromList
+      [ CueFile
+        { cueFileName = "Faithless - Live in Berlin.mp3"
+        , cueFileType = MP3
+        , cueFileTracks = NE.fromList
+          [ CueTrack
+            { cueTrackDigitalCopyPermitted = False
+            , cueTrackFourChannelAudio     = False
+            , cueTrackPreemphasisEnabled   = False
+            , cueTrackSerialCopyManagement = False
+            , cueTrackType                 = CueTrackAudio
+            , cueTrackIsrc                 = Nothing
+            , cueTrackTitle                = Just track1Title
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Just (CueTime 75)
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = NE.fromList [CueTime 0]
+            , cueTrackPostgap              = Nothing }
+          , CueTrack
+            { cueTrackDigitalCopyPermitted = False
+            , cueTrackFourChannelAudio     = False
+            , cueTrackPreemphasisEnabled   = False
+            , cueTrackSerialCopyManagement = False
+            , cueTrackType                 = CueTrackAudio
+            , cueTrackIsrc                 = Nothing
+            , cueTrackTitle                = Just track2Title
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Nothing
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = NE.fromList [CueTime 30150]
+            , cueTrackPostgap              = Nothing }
+          , CueTrack
+            { cueTrackDigitalCopyPermitted = False
+            , cueTrackFourChannelAudio     = False
+            , cueTrackPreemphasisEnabled   = False
+            , cueTrackSerialCopyManagement = False
+            , cueTrackType                 = CueTrackAudio
+            , cueTrackIsrc                 = Nothing
+            , cueTrackTitle                = Just track3Title
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Nothing
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = NE.fromList [CueTime 49050]
+            , cueTrackPostgap              = Just (CueTime 76) }
+          , CueTrack
+            { cueTrackDigitalCopyPermitted = False
+            , cueTrackFourChannelAudio     = False
+            , cueTrackPreemphasisEnabled   = False
+            , cueTrackSerialCopyManagement = False
+            , cueTrackType                 = CueTrackAudio
+            , cueTrackIsrc                 = Nothing
+            , cueTrackTitle                = Just track4Title
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Nothing
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = NE.fromList
+              [CueTime 76800, CueTime 76805]
+            , cueTrackPostgap              = Nothing }
+          , CueTrack
+            { cueTrackDigitalCopyPermitted = False
+            , cueTrackFourChannelAudio     = False
+            , cueTrackPreemphasisEnabled   = True
+            , cueTrackSerialCopyManagement = False
+            , cueTrackType                 = CueTrackAudio
+            , cueTrackIsrc                 = Nothing
+            , cueTrackTitle                = Just track5Title
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Nothing
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = NE.fromList [CueTime 115800]
+            , cueTrackPostgap              = Nothing }
+          , CueTrack
+            { cueTrackDigitalCopyPermitted = False
+            , cueTrackFourChannelAudio     = False
+            , cueTrackPreemphasisEnabled   = False
+            , cueTrackSerialCopyManagement = False
+            , cueTrackType                 = CueTrackAudio
+            , cueTrackIsrc                 = Nothing
+            , cueTrackTitle                = Just track6Title
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Nothing
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = NE.fromList [CueTime 138750]
+            , cueTrackPostgap              = Nothing }
+          , CueTrack
+            { cueTrackDigitalCopyPermitted = False
+            , cueTrackFourChannelAudio     = False
+            , cueTrackPreemphasisEnabled   = False
+            , cueTrackSerialCopyManagement = False
+            , cueTrackType                 = CueTrackAudio
+            , cueTrackIsrc                 = Nothing
+            , cueTrackTitle                = Just track7Title
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Nothing
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = NE.fromList [CueTime 172800]
+            , cueTrackPostgap              = Nothing }
+          , CueTrack
+            { cueTrackDigitalCopyPermitted = False
+            , cueTrackFourChannelAudio     = False
+            , cueTrackPreemphasisEnabled   = False
+            , cueTrackSerialCopyManagement = False
+            , cueTrackType                 = CueTrackAudio
+            , cueTrackIsrc                 = Nothing
+            , cueTrackTitle                = Just track8Title
+            , cueTrackPerformer            = Just performer
+            , cueTrackSongwriter           = Nothing
+            , cueTrackPregap               = Nothing
+            , cueTrackPregapIndex          = Nothing
+            , cueTrackIndices              = NE.fromList [CueTime 191625]
+            , cueTrackPostgap              = Nothing } ] } ] }
